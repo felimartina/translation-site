ebf8d99caa55320014448348361ddf374a22ec81
Felipe Martina <felipe.martina@3xmgroup.com>
2016-01-30 16:23:34 -0300

    Adding email messages for Contact Us Form.

--- TranslationsSite/Controllers/ContactUsController.cs
+++ TranslationsSite/Controllers/ContactUsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Net.Mail;
 using System.Text;
 using System.Threading;
 using System.Web.Mvc;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
 using Logging;
 using TranslationsSite.Helpers;
 
@@ -12,6 +14,7 @@ namespace TranslationsSite.Controllers
 {
     public class ContactUsController : Controller
     {
+        private const string SETTING_EMAIL_TO = "EmailTo";
         private static readonly Logger log = new Logger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public ActionResult Index()
@@ -24,30 +27,52 @@ namespace TranslationsSite.Controllers
             try
             {
                 //prepare email
-                var toAddress = ConfigurationManager.AppSettings["EmailTo"];
+                var toAddress = ConfigurationManager.AppSettings[SETTING_EMAIL_TO];
                 var fromAddress = email;
-                var subject = "You received an enquiry from " + name;
-                var sbMessage = new StringBuilder();
-
-                sbMessage.Append("Name: " + name + "\n");
-                sbMessage.Append("Email: " + email + "\n");
-                sbMessage.Append("Telephone: " + phone + "\n\n");
-                sbMessage.Append(message);
-
+                var subject = string.Format("You received an enquiry from {0}", name);
+                string htmlMessage;
+                using (var reader = new StreamReader(ControllerContext.HttpContext.Server.MapPath("~/Templates/EnquiryReceived.html")))
+                {
+                    htmlMessage = reader.ReadToEnd();
+                }
+                htmlMessage = htmlMessage.Replace("{{NAME}}", name)
+                                         .Replace("{{PHONE}}", phone)
+                                         .Replace("{{EMAIL}}", email)
+                                         .Replace("{{MESSAGE}}", message);
                 //start email Thread
                 var tEmail = new Thread(
-                    () => EmailHelper.SendEmail(toAddress, fromAddress, subject, sbMessage.ToString()));
+                    () => EmailHelper.SendEmail(toAddress, fromAddress, subject, htmlMessage));
                 tEmail.Start();
+                //Also send a confirmation email to the user
+                SendConfirmationEmail(name, email);
 
                 return JavaScript("true");
             }
             catch (Exception ex)
             {
                 log.Error("There was a problem trying to send an enquiry", ex);
+                log.Error("Enquiry Information: Name: {0}, Email: {1}, Phone: {2}, Message {3}",
+                    name, email, phone, message);
                 return JavaScript("false");
             }
 
         }
 
+        private void SendConfirmationEmail(string name, string email)
+        {
+            var toAddress = email;
+            var fromAddress = ConfigurationManager.AppSettings[SETTING_EMAIL_TO];
+            const string subject = "We have received your enquiry";
+            string htmlMessage;
+            using (var reader = new StreamReader(ControllerContext.HttpContext.Server.MapPath("~/Templates/WeHaveReceivedYourEnquiry.html")))
+            {
+                htmlMessage = reader.ReadToEnd();
+            }
+            htmlMessage = htmlMessage.Replace("{{NAME}}", name);
+            //start email Thread
+            var tEmail = new Thread(
+                () => EmailHelper.SendEmail(toAddress, fromAddress, subject, htmlMessage));
+            tEmail.Start();
+        }
     }
 }
---
+++ TranslationsSite/Helpers/EmailHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using Logging;
+
+namespace TranslationsSite.Helpers
+{
+    public class EmailHelper
+    {
+        private static readonly Logger log = new Logger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void SendEmail(string toAddress, string fromAddress, string subject, string message, bool html = true)
+        {
+            using (var smtpClient = 
+                new SmtpClient(
+                    ConfigurationManager.AppSettings["SmtpHost"], 
+                    Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"])))
+            {
+                smtpClient.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["SmtpEnableSsl"]);
+                smtpClient.UseDefaultCredentials = false;
+                var loginInfo = 
+                    new NetworkCredential(
+                        ConfigurationManager.AppSettings["SmtpUserName"], 
+                        ConfigurationManager.AppSettings["SmtpUserPass"]);
+                smtpClient.Credentials = loginInfo;
+
+                using (var mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(fromAddress);
+                    mail.To.Add(new MailAddress(toAddress));
+                    mail.Subject = subject;
+                    mail.Body = message;
+                    mail.IsBodyHtml = html;
+
+                    try
+                    {
+                        smtpClient.Send(mail);
+                        log.Info("Email successfully sent.");
+                    }
+                    catch (SmtpFailedRecipientsException ex)
+                    {
+                        foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+                        {
+                            var status = t.StatusCode;
+                            if (status == SmtpStatusCode.MailboxBusy ||
+                                status == SmtpStatusCode.MailboxUnavailable)
+                            {
+                                log.Error("Email Delivery failed - retrying in 5 seconds.");
+                                System.Threading.Thread.Sleep(5000);
+                                //resend
+                                smtpClient.Send(mail);
+                            }
+                            else
+                            {
+                                log.Error("Failed to deliver message to {0}", t.FailedRecipient);
+                            }
+                        }
+                    }
+                    catch (SmtpException Se)
+                    {
+                        // handle exception here
+                        log.Error(Se.ToString());
+                    }
+
+                    catch (Exception ex)
+                    {
+                        log.Error(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
